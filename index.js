32e1f0217a7c1c78027be052005cfa52cac8ae0f
Florian <info@ftws.de>
2019-02-28 21:26:26 +0100

    export functions

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var applyPath = require('./lib/applyPath');
+
 var copyPath = require('./lib/copyPath');
+var copyApplyPath = require('./lib/copyApplyPath');
 
-
+var movePath = require('./lib/movePath');
+var moveApplyPath = require('./lib/moveApplyPath');
 
 module.exports.applyPath = applyPath;
 module.exports.applyPathNumber = applyPath(Number);
 
-
 module.exports.copyPath = copyPath;
+module.exports.copyApplyPath = copyApplyPath;
+module.exports.copyApplyPathNumber = copyApplyPath(Number);
+
+module.exports.movePath = movePath;
+module.exports.moveApplyPath = moveApplyPath;
+module.exports.moveApplyPathNumber = moveApplyPath(Number);
